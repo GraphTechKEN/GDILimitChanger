bcf65e648245a69134629517214749c1a6c95765
GraphTechKEN <81954879+GraphTechKEN@users.noreply.github.com>
2024-07-25 20:40:50 +0900

    Add files via upload

---
+++ GDI_UpperChanger/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace GDI_UpperChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnDefault = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnMax = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbGDI = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 19);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnChange.Location = new System.Drawing.Point(150, 44);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(64, 23);
+            this.btnChange.TabIndex = 2;
+            this.btnChange.Text = "変更";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "10進";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(80, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(64, 19);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "16進";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "GDI オブジェクト数を変更するツールです";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(302, 12);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "免責：本ツールの使用によるいかなる損害・責任は負いかねます";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 12);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "プリセットを右側ボタンに配置しました";
+            // 
+            // btnDefault
+            // 
+            this.btnDefault.Location = new System.Drawing.Point(11, 18);
+            this.btnDefault.Name = "btnDefault";
+            this.btnDefault.Size = new System.Drawing.Size(64, 23);
+            this.btnDefault.TabIndex = 2;
+            this.btnDefault.Text = "デフォルト";
+            this.btnDefault.UseVisualStyleBackColor = true;
+            this.btnDefault.Click += new System.EventHandler(this.btnDefault_Click);
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.Location = new System.Drawing.Point(10, 44);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(64, 23);
+            this.btnDouble.TabIndex = 2;
+            this.btnDouble.Text = "倍";
+            this.btnDouble.UseVisualStyleBackColor = true;
+            this.btnDouble.Click += new System.EventHandler(this.btnDouble_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnMax);
+            this.groupBox1.Controls.Add(this.btnDouble);
+            this.groupBox1.Controls.Add(this.btnDefault);
+            this.groupBox1.Location = new System.Drawing.Point(237, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(84, 106);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "プリセット";
+            // 
+            // btnMax
+            // 
+            this.btnMax.Location = new System.Drawing.Point(10, 73);
+            this.btnMax.Name = "btnMax";
+            this.btnMax.Size = new System.Drawing.Size(64, 23);
+            this.btnMax.TabIndex = 2;
+            this.btnMax.Text = "最大";
+            this.btnMax.UseVisualStyleBackColor = true;
+            this.btnMax.Click += new System.EventHandler(this.btnMax_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnChange);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.tbGDI);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(14, 16);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(220, 106);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "GDIオブジェクト数上限設定";
+            // 
+            // tbGDI
+            // 
+            this.tbGDI.Location = new System.Drawing.Point(150, 81);
+            this.tbGDI.Name = "tbGDI";
+            this.tbGDI.ReadOnly = true;
+            this.tbGDI.Size = new System.Drawing.Size(64, 19);
+            this.tbGDI.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "GDIオブジェクト数(BVE6)";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 186);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "GDI オブジェクト数上限変更ツール";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnDefault;
+        private System.Windows.Forms.Button btnDouble;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnMax;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbGDI;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GDI_UpperChanger/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace GDI_UpperChanger
+{
+    public partial class Form1 : Form
+    {
+        private int GDIValue = 10000;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (IsAdministrator())
+            {
+                GDIValue = (int)Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows", "GDIProcessHandleQuota", "default");
+                textBox1.Text = GDIValue.ToString();
+                textBox2.Text = GDIValue.ToString("x4");
+            }
+            else
+            {
+                MessageBox.Show("管理者権限で実行してください、終了します");
+                this.Close();
+            }
+
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBox1.Text, out GDIValue);
+            if (GDIValue >= 256 && GDIValue <= 65535)
+            {
+                textBox2.Text = GDIValue.ToString("x4");
+                Microsoft.Win32.Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows", "GDIProcessHandleQuota", GDIValue);
+                MessageBox.Show("適用するにはPCを再起動してください");
+            }
+            else
+            {
+                MessageBox.Show("値は 256 から 65535 の範囲で指定してください");
+            }
+        }
+
+        private void btnDefault_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "10000";
+            btnChange_Click(sender, e);
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "65535";
+            btnChange_Click(sender, e);
+        }
+
+        private void btnDouble_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "20000";
+            btnChange_Click(sender, e);
+        }
+
+        public static bool IsAdministrator()
+        {
+            System.Security.Principal.WindowsIdentity wi =
+                System.Security.Principal.WindowsIdentity.GetCurrent();
+            System.Security.Principal.WindowsPrincipal wp =
+                new System.Security.Principal.WindowsPrincipal(wi);
+            return wp.IsInRole(
+                System.Security.Principal.WindowsBuiltInRole.Administrator);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] ps = Process.GetProcesses();
+            foreach (Process p in ps)
+            {
+                try
+                {
+                    if (p.ProcessName == "BveTs")
+                    {
+                        var exeName = "BveTs.exe";
+                        var pinfo = Process.GetProcessesByName(exeName);
+                        var procHandle = NativeMethods.OpenProcess((uint)NativeMethods.ProcessAccessFlags.All, false, (int)p.Id);
+                        var gdiObjCount = NativeMethods.GetGuiResources(procHandle, (uint)NativeMethods.GdiKind.GR_GDIOBJECTS);
+                        tbGDI.Text=gdiObjCount.ToString();
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+
+    public class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        public static extern uint GetGuiResources(IntPtr hProcess, uint uiFlags);
+
+        public enum GdiKind
+        {
+            GR_GDIOBJECTS = 0,
+            GR_USEROBJECTS = 1,
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr hObject);
+    }
+}
